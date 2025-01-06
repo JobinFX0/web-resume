424d2fcd72e1e138b2f358339d2b405a9b99828d
JobinFX0 <jobinkj842@gmail.com>
2025-01-06 21:25:52 +0530

    added navbar and components,router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
+import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import './App.css'
-import Navbar from './components/Navbar/Navbar.'
+import Navbar from './components/navBar/navBar'
+import Home from './components/pages/home'
+import About from './components/pages/about'
+import Contact from './components/pages/contact'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+      </Router>
       
     </>
   )
 }
+
+
+
 
 export default App
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom';
+
 const Navbar = () => {
   return (
    <nav className="Navbar">
     <ul>
-      <li> <a href="/">Home</a> </li>
-      <li> <a href="/about">About</a></li>
-      <li><a href="/contact">Contact Us</a></li>
+      <li> <Link to="/">Home</Link> </li>
+      <li> <Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact Us</Link></li>
     </ul>
    </nav>
   )
 }
-
 export default Navbar
 
 
