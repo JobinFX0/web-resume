f189ce36bed2b4a2b0baa25eae570a26bf7fdd85
JobinFX0 <jobinkj842@gmail.com>
2025-01-06 18:35:06 +0530

    fixed case-sensitive file naming issue

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav className="Navbar">
+    <ul>
+      <li> <a href="/">Home</a> </li>
+      <li> <a href="/about">About</a></li>
+      <li><a href="/contact">Contact Us</a></li>
+    </ul>
+   </nav>
+  )
+}
+
+export default Navbar
+
+
