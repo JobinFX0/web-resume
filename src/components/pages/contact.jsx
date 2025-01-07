df9d6c9c1dd77a82e12231b1b1980ac0c44945fd
JobinFX0 <jobinkj842@gmail.com>
2025-01-07 20:08:27 +0530

    done till contact page

--- src/components/pages/contact.jsx
+++ src/components/pages/contact.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact page</div>
+    <div className='contact'>
+      <h2 className='contact-items  h2'>GOT A MINUTE?</h2>
+      <h2 className='contact-items h2'>FEEL FREE TO CONTACT US</h2>
+      <p className='contact-items p'>mail@webresumeindia@gmail.com</p>
+    </div>
   )
 }
 
 export default Contact
+// CONTACT US
