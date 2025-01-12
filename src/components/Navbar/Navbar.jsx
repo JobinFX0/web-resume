b62f059d86a71690eb7f9b3f219c4b9bc8596c33
JobinFX0 <jobinkj842@gmail.com>
2025-01-12 17:03:52 +0530

    added on click login/logout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,17 +8,24 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
   const {loginWithRedirect,isAuthenticated,logout}= useAuth0();
 
+  const login=()=>{
+    loginWithRedirect();
+   }
+   const logoutt=()=>{
+    logout({ logoutParams: { returnTo: window.location.origin } })} 
+   
   return (
    <nav className="Navbar">
     <ul className='nav-links'>
       <li className='hover-nav'> <Link to="/">Home</Link> </li>
       <li className='hover-nav'> <Link to="/about">About</Link></li>
       <li className='hover-nav'><Link to="/contact">Contact Us</Link></li>
-      
+   
+
      {
-      isAuthenticated?(<li className='hover-nav'><Link to="/logout">Log out</Link></li>):(<li className='hover-nav'><Link to="/login">Log in</Link></li>)
+      isAuthenticated?(<li className='hover-nav'onClick={logoutt}>Log out</li>):(<li className='hover-nav' onClick={login}>Log in</li>)
      }
-      
+   
       
     </ul>
    </nav>
